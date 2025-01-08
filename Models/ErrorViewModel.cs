f3d37f94ead6d02c3f7c9d7a3314a20687972a1c
Matex2395 <mateorgafjo@gmail.com>
2025-01-08 12:15:38 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mini_Core_FiltroFechas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
