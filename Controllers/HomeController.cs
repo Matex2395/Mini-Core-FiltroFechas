09fa022b4bed0fd87afbd1f68c2366f4257b4a19
Matex2395 <mateorgafjo@gmail.com>
2025-01-08 16:03:18 -0500

    Agregar Controlador & Quitar Vista Privacy Se agregó el controlador del Core y se eliminó la vista de Privacy, así como su acceso en el footer.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mini_Core_FiltroFechas.Data;
 using Mini_Core_FiltroFechas.Models;
 
 namespace Mini_Core_FiltroFechas.Controllers
@@ -7,18 +9,15 @@ namespace Mini_Core_FiltroFechas.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MiniCoreGastosContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MiniCoreGastosContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
@@ -27,6 +26,32 @@ namespace Mini_Core_FiltroFechas.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FiltrarGastos(DateOnly fechaInicio, DateOnly fechaFin)
+        {
+            if (fechaInicio > fechaFin)
+            {
+                ModelState.AddModelError("", "La fecha de inicio no puede ser mayor a la fecha de fin.");
+                return View();
+            }
+
+            var resultado = await _context.Gastos
+                .Where(g => g.GastoFecha >= fechaInicio && g.GastoFecha <= fechaFin)
+                .GroupBy(g => g.GastoDepartamento!.DepartamentoNombre)
+                .Select(g => new
+                {
+                    Departamento = g.Key,
+                    Total = g.Sum(x => x.GastoMonto)
+                })
+                .ToListAsync();
+
+            ViewBag.Resultado = resultado;
+            ViewBag.FechaInicio = fechaInicio;
+            ViewBag.FechaFin = fechaFin;
+
+            return View();
         }
     }
 }
